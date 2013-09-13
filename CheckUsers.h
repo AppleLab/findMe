77967081449700d73ca90d28483bc1b62397ea6e
user <user@Mac-mini-administrator-8.local>
2013-09-13 14:53:18 +0400

    Change storyboard

---
+++ CheckUsers.h
@@ -0,0 +1,13 @@
+//
+//  CheckUsers.h
+//  По следам
+//
+//  Created by user on 13.09.13.
+//  Copyright (c) 2013 Ramil Garaev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CheckUsers : NSObject
+
+@end
---
+++ CheckUsers.m
@@ -0,0 +1,15 @@
+//
+//  CheckUsers.m
+//  По следам
+//
+//  Created by user on 13.09.13.
+//  Copyright (c) 2013 Ramil Garaev. All rights reserved.
+//
+
+#import "CheckUsers.h"
+
+@implementation CheckUsers
++(Bool *) CheckUsers: (NSString *) login, password{
+    
+}
+@end
