937a9bfcc2be9fc7d7d1d8f48a4b38d9cee0c1bc
user <user@Mac-mini-administrator-8.local>
2013-09-14 16:06:58 +0400

    1

--- CheckUsers.m
+++ CheckUsers.m
@@ -9,13 +9,5 @@
 #import "CheckUsers.h"
 
 @implementation CheckUsers
-/*+(BOOL *) CheckLogin: (NSString *)login andPassword:(NSString *) password{
-    //NSString *candidate = [NSString stringWithFormat:login, " ", password];
-    //hasPrefix - начинается ли указанная стркоа с подстроки..
-    //NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    //NSString *storePath = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"filename.txt"];
-    return YES;
-     
-}
- */
+
 @end
